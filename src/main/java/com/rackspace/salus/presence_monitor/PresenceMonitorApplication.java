09379983657468531b40b7a783f797a600dc473a
George Jahad <github@blackbirdsystems.net>
2018-11-27 17:01:11 -0800

    created from intellij

---
+++ src/main/java/com/rackspace/salus/presence_monitor/PresenceMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.rackspace.salus.presence_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceMonitorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rackspace/salus/presence_monitor/PresenceMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rackspace.salus.presence_monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PresenceMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
