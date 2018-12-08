e6aeba232659a93c256c5d59a1a39bc9244f6248
George Jahad <github@blackbirdsystems.net>
2018-12-07 19:46:37 -0800

    first tests working

--- src/test/java/com/rackspace/salus/telemetry/presence_monitor/services/PresenceMonitorProcessorTest.java
+++ src/test/java/com/rackspace/salus/telemetry/presence_monitor/services/PresenceMonitorProcessorTest.java
@@ -19,6 +19,7 @@ import com.rackspace.salus.telemetry.etcd.services.EnvoyResourceManagement;
 import com.rackspace.salus.telemetry.etcd.types.KeyRange;
 import com.rackspace.salus.telemetry.etcd.types.Keys;
 import com.rackspace.salus.telemetry.etcd.types.WorkAllocationRealm;
+import com.rackspace.salus.telemetry.model.ResourceInfo;
 import com.rackspace.salus.telemetry.presence_monitor.types.PartitionEntry;
 import io.etcd.jetcd.launcher.junit.EtcdClusterResource;
 import java.io.IOException;
@@ -85,33 +86,42 @@ public class PresenceMonitorProcessorTest {
 
 
   @Test
-  public void testProcessorStart() throws ExecutionException, InterruptedException {
+  public void testProcessorStart() throws Exception {
     doNothing().when(metricExporter).run();
     when(taskScheduler.submit((Runnable)any())).thenReturn(null);
 
     PresenceMonitorProcessor p = new PresenceMonitorProcessor(client, objectMapper,
             envoyResourceManagement,taskScheduler, metricExporter);
+    String resourceInfoString =
+            "{\"identifier\":\"os\",\"identifierValue\":\"LINUX\"," +
+            "\"labels\":{\"os\":\"LINUX\",\"arch\":\"X86_64\"},\"envoyId\":\"abcde\"," +
+            "\"tenantId\":\"123456\",\"address\":\"host:1234\"}";
 
     client.getKVClient().put(
-            ByteSequence.fromString("/resources/active/r1"),
-            ByteSequence.fromString(
-                    "{\"identifier\":\"os\",\"identifierValue\":\"LINUX\"," +
-                            "\"labels\":{\"os\":\"LINUX\",\"arch\":\"X86_64\"},\"envoyId\":\"abcde\"," +
-                            "\"tenantId\":\"123456\",\"address\":\"host:1234\"}")
-    ).join();
+            ByteSequence.fromString("/resources/active/1"),
+            ByteSequence.fromString(resourceInfoString)).join();
 
     client.getKVClient().put(
-            ByteSequence.fromString("/resources/expected/r1"),
-            ByteSequence.fromString(
-                    "{\"identifier\":\"os\",\"identifierValue\":\"LINUX\"," +
-                            "\"labels\":{\"os\":\"LINUX\",\"arch\":\"X86_64\"},\"envoyId\":\"abcde\"," +
-                            "\"tenantId\":\"123456\",\"address\":\"host:1234\"}")
-    ).join();
+            ByteSequence.fromString("/resources/expected/1"),
+            ByteSequence.fromString(resourceInfoString)).join();
+
+    String rangeStart = "0000000000000000000000000000000000000000000000000000000000000000",
+           rangeEnd   = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+           
     p.start("id1", "{" +
-            "\"start\":\"0000000000000000000000000000000000000000000000000000000000000000\"," +
-            "\"end\":\"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff\"}");
+            "\"start\":\"" + rangeStart + "\"," +
+            "\"end\":\"" + rangeEnd + "\"}");
 
     PartitionEntry partitionEntry = p.getPartitionTable().get("id1");
+    assertEquals("range start should be all zeros", rangeStart, partitionEntry.getRangeMin());
+    assertEquals("range end should be all f's", rangeEnd, partitionEntry.getRangeMax());
+
+    ResourceInfo resourceInfo = objectMapper.readValue(resourceInfoString, ResourceInfo.class);
+
+    PartitionEntry.ExpectedEntry expectedEntry = partitionEntry.getExpectedTable().get("1");
+    assertEquals(resourceInfo, expectedEntry.getResourceInfo());
+    assertEquals(true, expectedEntry.getActive());
+
     System.out.println("hi");
 
 
