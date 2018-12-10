11fa74877d1b54302a4d2920ab7c1290cccf048d
George Jahad <github@blackbirdsystems.net>
2018-12-09 16:13:34 -0800

    cleanup

--- src/main/java/com/rackspace/salus/telemetry/presence_monitor/PresenceMonitorApplication.java
+++ src/main/java/com/rackspace/salus/telemetry/presence_monitor/PresenceMonitorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PresenceMonitorApplication  {
-	public static void main(String[] args) {
-		SpringApplication.run(PresenceMonitorApplication.class, args);
+public class PresenceMonitorApplication {
 
-        }
+      public static void main(String[] args) {
+		SpringApplication.run(PresenceMonitorApplication.class, args);
+      }
 }
