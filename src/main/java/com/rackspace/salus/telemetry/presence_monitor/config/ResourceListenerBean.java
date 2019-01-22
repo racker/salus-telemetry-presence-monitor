3d93475610be26598afa415486926ce7b0c658f8
George Jahad <github@blackbirdsystems.net>
2019-01-21 19:56:54 -0800

    made resource listener a bean

---
+++ src/main/java/com/rackspace/salus/telemetry/presence_monitor/config/ResourceListenerBean.java
@@ -0,0 +1,54 @@
+/*
+ *    Copyright 2019 Rackspace US, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package com.rackspace.salus.telemetry.presence_monitor.config;
+
+
+import com.rackspace.salus.common.workpart.config.EtcdConfig;
+import com.rackspace.salus.common.workpart.config.EtcdProperties;
+import com.rackspace.salus.common.workpart.config.WorkerProperties;
+import com.rackspace.salus.common.workpart.services.WorkAllocator;
+import com.rackspace.salus.telemetry.presence_monitor.services.PresenceMonitorProcessor;
+import com.rackspace.salus.common.util.KeyHashing;
+import com.rackspace.salus.telemetry.etcd.services.EnvoyResourceManagement;
+import com.rackspace.salus.telemetry.presence_monitor.services.ResourceListener;
+import com.rackspace.salus.telemetry.presence_monitor.types.PartitionSlice;
+import org.apache.avro.io.EncoderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import com.coreos.jetcd.Client;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Configuration
+public class ResourceListenerBean {
+    static ConcurrentHashMap<String, PartitionSlice> partitionTable = new ConcurrentHashMap<>();
+    @Bean
+    public ConcurrentHashMap<String, PartitionSlice> getPartitionTable() {
+        return partitionTable;
+    }
+
+    @Bean
+    public ResourceListener getResourceListener() {
+        return new ResourceListener(partitionTable);
+    }
+}
