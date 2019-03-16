e747b170bd3eb6092220699e857261432052ad70
George Jahad <github@blackbirdsystems.net>
2019-03-15 20:31:56 -0700

    cleanup

--- src/test/java/com/rackspace/salus/telemetry/presence_monitor/services/ResourceListenerTest.java
+++ src/test/java/com/rackspace/salus/telemetry/presence_monitor/services/ResourceListenerTest.java
@@ -36,7 +36,6 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
